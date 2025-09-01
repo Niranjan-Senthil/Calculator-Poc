80d675d536f4914b754ab6df691fcf2a23623c8c
Varalakshmi <2438658@cognizant.com>
2025-09-01 21:43:25 +0530

    User Interface by vara

---
+++ Insurance.UI/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Insurance.UI.Models;
+
+namespace Insurance.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Login() => View();
+
+
+
+        [HttpPost]
+
+        public IActionResult Login(string username, string password) => RedirectToAction("Dashboard");
+        public IActionResult Register() => View();
+        [HttpPost]
+        public IActionResult Register(string username,string email, string password) => RedirectToAction("Dashboard");
+
+
+        public IActionResult Dashboard() => View();
+
+        public IActionResult ManageCustomers() => View();
+
+        public IActionResult AddCustomer() => View();
+
+        public IActionResult Policies() => View();
+
+        public IActionResult AddPolicy() => View();
+
+        public IActionResult Claims() => View();
+
+        public IActionResult AddClaim() => View();
+
+        public IActionResult Reports() => View();
+
+        public IActionResult GenerateReport() => View();
+    }
+}
---
+++ Insurance.UI/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Insurance.UI.Models;
+
+namespace Insurance.UI.Controllers { 
+
+
+    public class CustomerController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public IActionResult Login(string email, string password) => RedirectToAction("Dashboard");
+
+        public IActionResult Register() => View();
+        [HttpPost]
+        public IActionResult Register(string username, string email, string password) => RedirectToAction("Dashboard");
+
+        public IActionResult Dashboard() => View();
+
+        public IActionResult Profile() => View();
+
+        public IActionResult BuyPolicy() => View();
+        public IActionResult MyClaimsHistory() => View();
+        public IActionResult FileClaim() => View();
+    }
+}
